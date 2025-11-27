7c5f137bcbf3b53319fd69466aa452fb68ce776c
Zain Ul Abideen <zainsiddiqui1425@gmail.com>
2025-11-27 11:47:34 +0500

    Code for the Games Section
    This is the code for gaming Section in which there are many online games which you can play online by clicking play now button

---
+++ games.js
@@ -0,0 +1,60 @@
+function playGame1() {
+  window.open("https://www.friv.com/z/games/tictactoexxo/game.html");
+}
+function playGame2() {
+  window.open("https://www.friv.com/z/games/powerpamplona/game.html");
+}
+function playGame3() {
+  window.open("https://www.friv.com/z/games/frivwordsearch/game.html");
+}
+function playGame4() {
+  window.open("https://www.friv.com/z/games/deathchase/game.html");
+}
+function playGame5() {
+  window.open("https://www.friv.com/z/games/bloxorz/game.html");
+}
+function playGame6() {
+  window.open("https://www.friv.com/z/games/colorlink/game.html");
+}
+function playGame7() {
+  window.open("https://www.friv.com/z/games/badicecream/game.html");
+}
+function playGame8() {
+  window.open("https://www.friv.com/z/games/zumbaquest/game.html");
+}
+function playGame9() {
+  window.open("https://www.friv.com/z/games/truckloader/game.html");
+}
+function playGame10() {
+  window.open("https://www.friv.com/z/games/happycups/game.html");
+}
+function playGame11() {
+  window.open("https://www.friv.com/z/games/sleepwalk/game.html");
+}
+function playGame12() {
+  window.open("https://www.friv.com/z/games/ludolegend/game.html");
+}
+function playGame13() {
+  window.open("https://www.friv.com/z/games/bigheadfootball/game.html");
+}
+function playGame14() {
+  window.open("https://www.friv.com/z/games/jellygo/game.html");
+}
+function playGame15() {
+  window.open("https://www.friv.com/z/games/shapeshifter/game.html");
+}
+function playGame16() {
+  window.open("https://www.friv.com/z/games/papaspizzeria/game.html");
+}
+function playGame17() {
+  window.open("https://www.friv.com/z/games/frivcoloring/game.html");
+}
+function playGame18() {
+  window.open("https://www.friv.com/z/games/blocktower/game.html");
+}
+function playGame19() {
+  window.open("https://www.friv.com/z/games/blocktower/game.html");
+}
+function playGame20() {
+  window.open("https://www.friv.com/z/games/pacxon/game.html");
+}
